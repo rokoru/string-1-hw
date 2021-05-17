5fceb60041505a49c913a112e562eb08ef4ae13a
Dmitry <gomerty15@mail.ru>
2021-05-17 19:02:28 +0300

    добавлено решение

---
+++ zadacha string 1/zadacha string 1/zadacha string 1.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <cstring>
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    const char* a = "Лондон из зе кэпитал оф греат бритаин";
+    int e = 0;
+    for (int i = 0; i < std::strlen(a); ++i) {
+        if (a[i] == 'о') {
+            e += 1;
+        }
+        else if (a[i] == ' ') {
+            break;
+        }
+    }
+    std::cout << e << std::endl;
+}
+
